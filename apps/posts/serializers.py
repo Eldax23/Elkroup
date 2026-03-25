8e851295d22aedeb27fb6d14c0a1e89da0de6ffd
Eldax <mohamedabu.el3ela@gmail.com>
2026-03-26 00:01:33 +0200

    feat: adding CommentSerializer

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post , Comment
 from apps.users.serializers import UserSerializer
 
 # main post serializer responsible for showing/updating posts
@@ -21,4 +21,12 @@ class CreatePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id' , 'content' , 'image')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    class Meta:
+        model = Comment
+        fields = ('id',  'author' , 'content' , 'created_at')
+        read_only_fields = ('id' , 'author' , 'created_at')
 
