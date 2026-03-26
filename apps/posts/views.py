3e0fab9917314a3a12a695d5359dcb43748c80f4
Eldax <mohamedabu.el3ela@gmail.com>
2026-03-26 20:02:51 +0200

    feat: Adding Feedview for getting posts based on user follows

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
+
 
 # Create your views here.
+
+
+# Feedview (get the posts of the people you're currently following)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        # get the currently logged in user
+        user = self.request.user
+
+        # get every follow object with this user as the follower
+        followings = user.following.all()
+
+        # extract those people that our user follows
+        followed_users = [f.following for f in followings]
+
+        # get the posts of those people
+        posts = Post.objects.filter(author__in=followed_users)
+        return posts
