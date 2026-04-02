b5c2508ffad7f2e91fb6fe9d4f0de5abefe82595
Eldax <mohamedabu.el3ela@gmail.com>
2026-04-02 04:33:40 +0200

    feat: implementing recieve() to handle incoming messages

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -1,6 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import Room
+from .models import Room , Message
 import json
 
 
@@ -49,7 +49,92 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.channel_layer.group_discard(self.room_group , self.channel_name)
 
 
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        # get type of message , chat_message , edit_message , delete_message
+        msg_type = data.get('type')
 
+        if msg_type == 'message':
+            content = data.get('content' , None).strip()
+
+            if not content:
+                # user sent an empty message
+                return
+            
+            # save message to db
+            message = await self.save_message_in_db(content)
+            await self.channel_layer.group_send(self.room_group , {
+                'type': 'chat_message',
+                'id': message.id,
+                'content': message.content,
+                'sender': self.user,
+                'created_at':message.created_at 
+            })
+
+        elif msg_type == 'typing':
+            # send a request that triggers the typing_indication event handler function
+            await self.channel_layer.group_send(self.room_group , {
+                'type': 'typing_indicator',
+                'user_id': self.user.id,
+                'username': self.user.username,
+                'is_typing': True
+            })
+        
+        elif msg_type == 'edit':
+            msg_id = data.get['message_id']
+            newMessage = data.get['new_message']
+            if msg_id:
+                msg = await edit_message_in_db(msg_id=msg_id , newMessage=newMessage)
+                # triggers the message_edited event handler
+                await self.channel_layer.group_send(self.room_group , {
+                    'type': 'message_edited',
+                    'message_id': msg_id,
+                    'new_message': newMessage
+                })
+
+        elif msg_type == 'delete':
+            msg_id = data.get['message_id']
+            if msg_id:
+                msg - await delete_message_in_db(msg_id=msg_id)
+                # triggers the message_deleted event handler
+                await self.channel_layer.group_send(self.room_group , {
+                    'type': 'message_deleted',
+                    'message_id': msg_id,
+                })
+
+
+
+
+# check whether a user a member of this room or no
 @database_sync_to_async
 def is_member(self):
     return Room.objects.filter(pk=self.room_id , members=self.user).exists()
+
+@database_sync_to_async
+def save_message_in_db(self , content):
+    # get the current room
+    curr_room = Room.objects.get(pk=self.room_id)
+
+    return Message.objects.create(room=curr_room , sender=self.user , content=content)
+
+
+@database_sync_to_async
+def edit_message_in_db(self , msg_id , newMessage):
+    try: 
+        message = Message.object.filter(pk=msg_id , sender=self.user)
+        message.content = newMessage
+        message.save()
+        return True
+    except Message.DoesNotExist:
+        return False
+
+@database_sync_to_async
+def delete_message_in_db(self , msg_id):
+    try:
+        message = Message.objects.filter(pk=msg_id , sender=self.user)
+        message.delete()
+        return True
+    except Message.DoesNotExist:
+        return False
+    
+
