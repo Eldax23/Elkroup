87e44b6ba54e55139c6a27aa1de051ac0ad029ca
Eldax <mohamedabu.el3ela@gmail.com>
2026-04-21 01:04:24 +0200

    feat: configure url endpoints for chat

---
+++ apps/chat/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('rooms/' , views.RoomListView.as_view(), name='rooms'),
+    path('rooms/<int:pk>/' , views.RoomDetailView.as_view() , name='rooms detail'),
+    path('rooms/<int:room_id>/messages/' , views.MessageHistoryView.as_view() , name='message history'),
+    path('rooms/dm/' , views.create_dm , name='create-dm')
+]
