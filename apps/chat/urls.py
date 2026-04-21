c89439632801541c6c04664f26988233a692fb86
Eldax <mohamedabu.el3ela@gmail.com>
2026-04-21 08:42:50 +0200

    feat: configure url endpoints for the create_group logic

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('rooms/' , views.RoomListView.as_view(), name='rooms'),
     path('rooms/<int:pk>/' , views.RoomDetailView.as_view() , name='rooms detail'),
     path('rooms/<int:room_id>/messages/' , views.MessageHistoryView.as_view() , name='message history'),
-    path('rooms/dm/' , views.create_dm , name='create-dm')
+    path('rooms/dm/' , views.create_dm , name='create-dm'),
+    path('rooms/group/' , views.create_group , name='create-group')
 ]
